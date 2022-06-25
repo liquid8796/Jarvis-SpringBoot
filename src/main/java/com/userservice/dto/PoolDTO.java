9d915d9a6d70b324f9abeab2f8eca9d04b49767a
liquid8796 <hanam.tranle.5@gmail.com>
2022-06-25 10:33:28 +0700

    25/06/2022 10:33

--- src/main/java/com/userservice/dto/PoolDTO.java
+++ src/main/java/com/userservice/dto/PoolDTO.java
@@ -3,9 +3,11 @@ package com.userservice.dto;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.io.Serializable;
+
 @Getter
 @Setter
-public class PoolDTO {
+public class PoolDTO implements Serializable {
     private String ip;
     private String name;
     private int port;
