387d45cad4f06abfd7480251f794a69312cc6538
liquid8796 <hanam.tranle.5@gmail.com>
2022-06-25 10:35:30 +0700

    25/06/2022 10:35

--- src/main/java/com/userservice/controller/TestController.java
+++ src/main/java/com/userservice/controller/TestController.java
@@ -2,6 +2,7 @@ package com.userservice.controller;
 
 import com.userservice.dto.PoolDTO;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/test")
 public class TestController {
 
-    @GetMapping("/header")
+    @GetMapping(value = "/header", produces = { MediaType.APPLICATION_JSON_VALUE })
     public PoolDTO getHeader(){
         PoolDTO result = new PoolDTO();
         result.setIp("sg.minexmr.com");
